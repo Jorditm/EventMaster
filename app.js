f7ba73f226715930b7bbf658a0e488bada57d18c
Sergi Botargues <sergibotargues@MacBook-Air-de-Sergi.local>
2020-08-02 21:13:23 +0200

    domingo

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 require('dotenv').config();
 
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
 const bodyParser = require('body-parser');
 const favicon = require('serve-favicon');
 const hbs = require('hbs');
 const mongoose = require('mongoose');
 
+const session = require('express-session'); 
+const MongoStore = require('connect-mongo')(session); 
+
+const indexRouter = require('./routes/index');
+const authRouter = require('./routes/auth');
+
+
+
+
 mongoose
-  .connect('mongodb://localhost/library-project', {
+  .connect('mongodb://localhost/EventMaster', {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
@@ -25,10 +34,8 @@ mongoose
     console.error('Error connecting to mongo', err);
   });
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -37,18 +44,42 @@ app.set('view engine', 'hbs');
 // Middleware Setup
 
 app.use(logger('dev'));
-app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+  secret: 'never do your own EventMaster again',
+  resave: true,
+  saveUninitialized: true,
+  cookie: { maxAge: 60000 },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  })
+}));
+
+app.use((req, res, next) => {
+  if (req.session.currentUser) {
+    res.locals.currentUserInfo = req.session.currentUser;
+    res.locals.isUserLoggedIn = true;
+  } else {
+    res.locals.isUserLoggedIn = false;
+  }
+
+  next();
+});
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', authRouter);
+
+
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
